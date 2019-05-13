72197cdb3108d6251d93fd34583de1fa2d32ec63
klaus <oriokiklaus@gmail.com>
2019-05-13 11:16:33 +0300

    add classmethods to allow for the search functionallity in the app

--- pics/models.py
+++ pics/models.py
@@ -28,4 +28,49 @@ class Image (models.Model):
     image_category = models.ForeignKey(Category)
     pub_date = models.DateTimeField(auto_now_add=True)
     post_image = models.ImageField(upload_to = 'my-photos/', null=True)
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
     
+    @classmethod
+    def update_image(cls, image_id, **kwargs):
+        rows = 0
+        if kwargs is not None:
+            rows = cls.objects.filter(id = image_id).update(**kwargs)
+
+        return rows
+
+    # def get_image_by_id(self):
+    #     self.objects.get(id)
+
+
+    #to specify model-specific options.
+    class Meta:
+        ordering = ['image_name']   
+
+    @classmethod
+    def todays_pics(cls):
+        today = dt.date.today()
+        pics = cls.objects.filter(pub_date__date = today)
+        return pics
+
+    @classmethod
+    def days_pics(cls,date):
+        pics = cls.objects.filter(pub_date__date = date)
+        return pics
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        pics = cls.objects.filter(image_category__name__icontains=search_term)
+        return pics
+
+    @classmethod
+    def search_by_location(cls,search_term):
+        pics = cls.objects.filter(image_location__name__icontains=search_term)
+        return pics
