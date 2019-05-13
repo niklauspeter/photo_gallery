330002016ef25db51b2869cf082571afd05acb55
klaus <oriokiklaus@gmail.com>
2019-05-13 10:54:20 +0300

    add view functions and render them to their respective templates

--- pics/views.py
+++ pics/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse, Http404
 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
+
+
+def past_days_pics(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(pics_of_day)
+
+    return render(request, 'all-pics/past-pics.html', {"date": date})
+
+def pics_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-pics/today-pics.html', {"date": date,})
