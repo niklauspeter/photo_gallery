f620f17cf589a7e78b77cc7f07524a53200482aa
klaus <oriokiklaus@gmail.com>
2019-05-13 12:01:34 +0300

    add filter horizontal property to order manytomany fields

--- pics/admin.py
+++ pics/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Category,Areas,Image,tags
 
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
 admin.site.register(Category)
 admin.site.register(Areas)
-admin.site.register(Image)
+admin.site.register(Image,ImageAdmin )
 admin.site.register(tags)
