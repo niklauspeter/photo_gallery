3c2860ef8244aa06b245a00879ac454e7dd1a9d4
klaus <oriokiklaus@gmail.com>
2019-05-13 10:50:11 +0300

    create a path for django to find the static file

--- photos/settings.py
+++ photos/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pics.apps.PicsConfig',
+    # 'pics',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
