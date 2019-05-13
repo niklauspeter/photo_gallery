089ec87a09759a3ca86bc3648a9bce45f7d50897
klaus <oriokiklaus@gmail.com>
2019-05-13 11:58:38 +0300

    set route to load when site root is requested

--- pics/admin.py
+++ pics/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category,Areas,Image,tags
+
+admin.site.register(Category)
+admin.site.register(Areas)
+admin.site.register(Image)
+admin.site.register(tags)
--- pics/urls.py
+++ pics/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.pics_of_day, name='picsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_pics,name = 'pastPics'),
     url('^today$',views.pics_of_day, name='picsToday')
 ]
