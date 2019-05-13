312f9b4a307bfd068c16453690b37e682703b04d
klaus <oriokiklaus@gmail.com>
2019-05-13 11:15:36 +0300

    add model classes and their respective attributes representing collumns

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+
+
+class Areas (models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def save_area(self):
+        self.save()
+        
+class Image (models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.CharField(max_length =200)
+    image_location = models.ForeignKey(Areas)
+    image_category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    post_image = models.ImageField(upload_to = 'my-photos/', null=True)
+    
