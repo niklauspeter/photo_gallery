c94550fa5eb50d9650802408424edf552c46b5db
klaus <oriokiklaus@gmail.com>
2019-05-14 16:23:43 +0300

    add script to copy immage link to clipboard

--- pics/urls.py
+++ pics/urls.py
@@ -8,7 +8,9 @@ urlpatterns=[
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_pics,name = 'pastPics'),
     url('^today$',views.pics_of_day, name='picsToday'),
     url('^home$',views.gallery_pics, name='allPics'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    # url('^admin$',name='admin')
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
